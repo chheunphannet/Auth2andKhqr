1727c77a2bbd5ea4730390dea4e94b7bb066c79b
Chheun Phanet <chhernphannet0001@gmail.com>
2025-10-24 23:06:00 +0700

    Initial commit

---
+++ src/main/java/com/kh/auth2andkhqr/Auth2andkhqrApplication.java
@@ -0,0 +1,13 @@
+package com.kh.auth2andkhqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth2andkhqrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth2andkhqrApplication.class, args);
+	}
+
+}
